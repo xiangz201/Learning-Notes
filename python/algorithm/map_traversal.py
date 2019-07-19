d204105fa14d22ddd8974dca315388899a37c090
xiangz201 <1123443786@qq.com>
2019-07-19 19:59:24 +0800

    树的bfs和dfs

---
+++ python/algorithm/map_traversal.py
@@ -0,0 +1,55 @@
+# -*-coding:utf-8-*-
+"""
+图的广义优先遍历（bfs）和深度优先遍历（dfs）
+广义优先遍历就是从根节点出发，按层横向遍历。使用的是一个队列.
+深度优先遍历是先序遍历的推广，从某个定点出发，一直遍历到最深处，然后回溯找到没有遍历到的点，再次应用深度优先遍历。使用的是一个栈。
+=============================
+bfs(node_root)                 广义优先遍历，返回遍历结果列表
+dfs(node_root)                 深度优先遍历，返回遍历结果列表
+=============================
+"""
+from queue import Queue
+
+
+class node(object):
+    def __init__(self, data=None, next=None):
+        self.next = next
+        self.data = data
+
+
+def bfs(node):
+    result = []
+    if node is None:
+        return
+    queue = Queue()
+    label = set()
+    queue.put(node)
+    label.add(node)
+    while not queue.empty():
+        cur = queue.get()
+        result.append(cur.data)
+        for next in cur.next:
+            if next not in label:
+                queue.put(next)
+                label.add(next)
+    return result
+
+
+def dfs(node):
+    result = []
+    if node is None:
+        return
+    label = set()
+    stack = []
+    label.add(node)
+    stack.append(node)
+    while len(stack)>0:
+        cur = stack.pop()
+        if cur not in label:
+            label.add(cur)
+            result.append(cur.value)
+        for next in cur.next:
+            if next not in label:
+                stack.append(next)
+    return result
+
