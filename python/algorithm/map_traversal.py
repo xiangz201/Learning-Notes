ddb99caa840f9cb8fcc3514cd2a933a676b9ef15
xiangz201 <1123443786@qq.com>
2019-07-20 14:11:27 +0800

    树的bfs和dfs

--- python/algorithm/map_traversal.py
+++ python/algorithm/map_traversal.py
@@ -4,8 +4,9 @@
 广义优先遍历就是从根节点出发，按层横向遍历。使用的是一个队列.
 深度优先遍历是先序遍历的推广，从某个定点出发，一直遍历到最深处，然后回溯找到没有遍历到的点，再次应用深度优先遍历。使用的是一个栈。
 =============================
-bfs(node_root)                 广义优先遍历，返回遍历结果列表
-dfs(node_root)                 深度优先遍历，返回遍历结果列表
+bfs(node_root)                   广义优先遍历，返回遍历结果列表
+dfs(node_root)                   深度优先遍历，非递归方法，返回遍历结果列表
+dfs_iter(G, cur, label = set())  深度优先遍历，递归方法，返回遍历结果列表
 =============================
 """
 from queue import Queue
@@ -27,7 +28,7 @@ def bfs(node):
     label.add(node)
     while not queue.empty():
         cur = queue.get()
-        result.append(cur.data)
+        result.append(cur.value)
         for next in cur.next:
             if next not in label:
                 queue.put(next)
@@ -52,4 +53,14 @@ def dfs(node):
             if next not in label:
                 stack.append(next)
     return result
+
+
+def dfs_inter(G, cur, label=set()):
+    print(cur.value)
+    label.add(cur)
+    #G(cur)表示cur的相邻顶点    
+    for u in G(cur):
+        if u not in label:
+            dfs(G,u,label)
+        
 
